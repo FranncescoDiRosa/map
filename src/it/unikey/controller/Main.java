73c74a05f1c00631a90798b1abe53097ded4e3e2
davide_piecek <dado.pck@hotmail.it>
2022-02-04 17:58:14 +0100

    In lavorazione

--- src/it/unikey/controller/Main.java
+++ src/it/unikey/controller/Main.java
@@ -1,15 +1,29 @@
 package it.unikey.controller;
 
 import com.sun.org.apache.xpath.internal.operations.Variable;
+
 import it.unikey.entities.Corso;
 import it.unikey.entities.Studente;
 
+import java.net.CacheRequest;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws Exception {
         Corso c = new Corso("chimica organica 2", "lucio pellacani", 9);
+        Corso d = new Corso("istituzioni di matematica 1", "ada ardito", 6);
+        Corso e = new Corso("chimica fisica 1", "victor malus", 6);
+        Corso f = new Corso("fisica 2", "mario rossi", 9);
 
-        System.out.println(c.getCodice());
+        Studente g = new Studente("1234567890123457", "Davide", "Piecek");
+        Studente h = new Studente("1234567890123458", "Sandro", "Pertini");
+        Studente i = new Studente("1234567890123459", "Toto", "Riina");
+        Studente j = new Studente("1234567890123450", "Giuseppe", "Garibaldi");
+        Studente k = new Studente("1234567890123451", "Karol", "Wojtila");
+        Studente l = new Studente("1234567890123452", "Maria", "Di Nazaret");
+
+
     }
 }
--- src/it/unikey/entities/Esame.java
+++ src/it/unikey/entities/Esame.java
@@ -1,12 +1,56 @@
 package it.unikey.entities;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Esame
 {
+    private String codice;
     private Date dataEsame;
     private int voto;
     private boolean lode;
 
-    
+    public Esame(Date dataEsame, int voto, boolean lode, Studente studente, Corso corso)
+    {
+        this.dataEsame = dataEsame;
+        this.voto = voto;
+        this.lode = lode;
+        this.codice = generaCodiceEsame(studente, corso, dataEsame);
+    }
+
+    private String generaCodiceEsame(Studente s, Corso corso, Date dataEsame)
+    {
+        return s.getMatricola()+"_"+corso.getCodice()+"_"+dataEsame.toString();
+    }
+
+    public String getCodice() {
+        return codice;
+    }
+
+    public Date getDataEsame() {
+        return dataEsame;
+    }
+
+    public int getVoto() {
+        return voto;
+    }
+
+    public boolean isLode() {
+        return lode;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj ==  null)
+            return false;
+        if(obj == this)
+            return true;
+        if(obj instanceof Esame)
+        {
+            Esame e = (Esame) obj;
+            return this.codice.equals(e.codice);
+        }
+        return false;
+    }
 }
